bab86d24b889bb31ea1c2609e84bbe7295768358
Kristýna Nováková <novakovkri@gmail.com>
2023-08-19 20:38:58 +0200

    Sensor Location

--- src/pages/SensorMap/SensorMap.jsx
+++ src/pages/SensorMap/SensorMap.jsx
@@ -2,6 +2,13 @@ import React, { useRef, useEffect } from 'react';
 import { Map, View } from 'ol';
 import { Tile } from 'ol/layer';
 import { OSM } from 'ol/source';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { Style, Circle, Fill } from 'ol/style';
+import '../App/app.css';
 
 const SensorMap = ({ coordinates }) => {
   const mapRef = useRef(null);
@@ -12,14 +19,35 @@ const SensorMap = ({ coordinates }) => {
         target: mapRef.current,
         layers: [new Tile({ source: new OSM() })],
         view: new View({
-          center: coordinates,
+          center: fromLonLat(coordinates),
           zoom: 12,
         }),
       });
+
+      const sensorFeature = new Feature({
+        geometry: new Point(fromLonLat(coordinates)),
+      });
+
+      sensorFeature.setStyle(
+        new Style({
+          image: new Circle({
+            radius: 6,
+            fill: new Fill({ color: 'red' }),
+          }),
+        }),
+      );
+
+      const vectorLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [sensorFeature],
+        }),
+      });
+
+      map.addLayer(vectorLayer);
     }
   }, [coordinates]);
 
-  return <div ref={mapRef} style={{ width: '100%', height: '400px' }} />;
+  return <div ref={mapRef} style={{ width: '100%', height: '100%' }} />;
 };
 
 export default SensorMap;
